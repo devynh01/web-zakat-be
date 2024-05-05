205fb7f278dc0ca899001a4b47d2637d897860c9
Shidqi Amirul Haq <shidqiamirulhaq@gmail.com>
2024-05-05 22:30:34 +0700

    feat: add supabase & prisma orm

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(cors<Request>());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
